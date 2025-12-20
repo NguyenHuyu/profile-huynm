f588eb6ae452ae3110e4eb8434081430cee78433
NguyenHuyu <yuhuynguyen.1204@gmail.com>
2025-12-20 18:56:43 +0700

    fix: image generate

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,28 +16,28 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "https://scontent.fsgn8-4.fna.fbcdn.net/v/t39.30808-6/592534066_2353918168360199_958764074878040352_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=yf6oOTT9XXoQ7kNvwEUvnLn&_nc_oc=Admpn0Z8c87e052-citgMTkf_bWAB2UQ1y0BjmZEXP8lx-WU0gVUvYHR6jJNrCglHxLKOoqG_6wzGA16OT38qP_l&_nc_zt=23&_nc_ht=scontent.fsgn8-4.fna&_nc_gid=3x_cTcmMccinS-C2sH1dLQ&oh=00_AfkYoKBP07QJ64KMhtsaxZaXdyaxh7Z2KoZcFYQubFn8Vg&oe=6938239D",
+        url: "https://scontent.fsgn19-1.fna.fbcdn.net/v/t39.30808-6/598974846_2368969780188371_3888231183088188503_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeEFgfaI_vYoRh_zAGFab7TUb-dCq18lG4lv50KrXyUbiRLdyWHv1j8hONMoZD7zHpOrteWJ2Rf9uoHZYNi25f0_&_nc_ohc=yF47WG2XK5MQ7kNvwFCPkhU&_nc_oc=Adk8SDrjcxQ1lrapZw_Nw9gi83iq_B3GjaOwbzQ3vmvu-tfPIl337s-Jih71fSBN-DM&_nc_zt=23&_nc_ht=scontent.fsgn19-1.fna&_nc_gid=Gf9sjAC7qOFSQ99fYl7aOg&oh=00_AfnMBvay5khjWoqt-WEa7dQ0iTjHdBYwew7wdvHxcuB7SQ&oe=694C74B2",
       },
     ],
   },
 
   openGraph: {
-    title: "Nguyễn Minh Huy | Full-Stack Developer",
+    title: "Nguyễn Minh Huy | Full-Stack Software Engineer",
     description:
-      "Full-stack developer with expertise in React, Next.js, Node.js, and .NET.",
+      "Full-stack software engineer with expertise in React, Next.js, Node.js, and .NET.",
     type: "website",
     images: [
-      "https://scontent.fsgn8-4.fna.fbcdn.net/v/t39.30808-6/592534066_2353918168360199_958764074878040352_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=yf6oOTT9XXoQ7kNvwEUvnLn&_nc_oc=Admpn0Z8c87e052-citgMTkf_bWAB2UQ1y0BjmZEXP8lx-WU0gVUvYHR6jJNrCglHxLKOoqG_6wzGA16OT38qP_l&_nc_zt=23&_nc_ht=scontent.fsgn8-4.fna&_nc_gid=3x_cTcmMccinS-C2sH1dLQ&oh=00_AfkYoKBP07QJ64KMhtsaxZaXdyaxh7Z2KoZcFYQubFn8Vg&oe=6938239D",
+      "https://scontent.fsgn19-1.fna.fbcdn.net/v/t39.30808-6/598974846_2368969780188371_3888231183088188503_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeEFgfaI_vYoRh_zAGFab7TUb-dCq18lG4lv50KrXyUbiRLdyWHv1j8hONMoZD7zHpOrteWJ2Rf9uoHZYNi25f0_&_nc_ohc=yF47WG2XK5MQ7kNvwFCPkhU&_nc_oc=Adk8SDrjcxQ1lrapZw_Nw9gi83iq_B3GjaOwbzQ3vmvu-tfPIl337s-Jih71fSBN-DM&_nc_zt=23&_nc_ht=scontent.fsgn19-1.fna&_nc_gid=Gf9sjAC7qOFSQ99fYl7aOg&oh=00_AfnMBvay5khjWoqt-WEa7dQ0iTjHdBYwew7wdvHxcuB7SQ&oe=694C74B2",
     ],
   },
 
   twitter: {
     card: "summary_large_image",
-    title: "Nguyễn Minh Huy | Full-Stack Developer",
+    title: "Nguyễn Minh Huy | Full-Stack Software Engineer",
     description:
-      "Full-stack developer with expertise in React, Next.js, Node.js, and .NET.",
+      "Full-stack software engineer with expertise in React, Next.js, Node.js, and .NET.",
     images: [
-      "https://scontent.fsgn8-4.fna.fbcdn.net/v/t39.30808-6/592534066_2353918168360199_958764074878040352_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=yf6oOTT9XXoQ7kNvwEUvnLn&_nc_oc=Admpn0Z8c87e052-citgMTkf_bWAB2UQ1y0BjmZEXP8lx-WU0gVUvYHR6jJNrCglHxLKOoqG_6wzGA16OT38qP_l&_nc_zt=23&_nc_ht=scontent.fsgn8-4.fna&_nc_gid=3x_cTcmMccinS-C2sH1dLQ&oh=00_AfkYoKBP07QJ64KMhtsaxZaXdyaxh7Z2KoZcFYQubFn8Vg&oe=6938239D",
+      "https://scontent.fsgn19-1.fna.fbcdn.net/v/t39.30808-6/598974846_2368969780188371_3888231183088188503_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeEFgfaI_vYoRh_zAGFab7TUb-dCq18lG4lv50KrXyUbiRLdyWHv1j8hONMoZD7zHpOrteWJ2Rf9uoHZYNi25f0_&_nc_ohc=yF47WG2XK5MQ7kNvwFCPkhU&_nc_oc=Adk8SDrjcxQ1lrapZw_Nw9gi83iq_B3GjaOwbzQ3vmvu-tfPIl337s-Jih71fSBN-DM&_nc_zt=23&_nc_ht=scontent.fsgn19-1.fna&_nc_gid=Gf9sjAC7qOFSQ99fYl7aOg&oh=00_AfnMBvay5khjWoqt-WEa7dQ0iTjHdBYwew7wdvHxcuB7SQ&oe=694C74B2",
     ],
   },
 };
